3061291d83ad9270785d5b20ef82a5a8398cfc9a
Alberto Bollo <albertobollo7@protonmail.com>
2026-02-15 21:26:00 +0100

    chore: Update hibernate.cfg.xml and added User model for testing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,12 @@ module org.example.studentgrades {
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
+    requires static lombok;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires java.naming;
+
+    opens models to org.hibernate.orm.core;
 
     opens org.example.studentgrades to javafx.fxml;
     exports org.example.studentgrades;
--- src/main/java/org/example/studentgrades/Launcher.java
+++ src/main/java/org/example/studentgrades/Launcher.java
@@ -1,9 +1,35 @@
 package org.example.studentgrades;
 
 import javafx.application.Application;
+import models.User;
+import models.UserType;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
+
+        User user = new User(
+                "alberto",
+                "1234",
+                "Alberto",
+                "Bollo", UserType.ESTUDIANTE);
+
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+        Session session = sf.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(user);
+
+        transaction.commit();
+
+        session.close();
+        sf.close();
+
     }
 }
