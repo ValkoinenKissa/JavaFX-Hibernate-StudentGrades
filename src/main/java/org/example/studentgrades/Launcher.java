01c81b80ed3db34c3364f5f3e4e6af891a6ce89e
Alberto Bollo <albertobollo7@protonmail.com>
2026-02-16 21:59:13 +0100

    chore: Delete ORM test

--- src/main/java/org/example/studentgrades/Launcher.java
+++ src/main/java/org/example/studentgrades/Launcher.java
@@ -1,35 +1,10 @@
 package org.example.studentgrades;
 
 import javafx.application.Application;
-import models.User;
-import models.UserType;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
 
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
-
-        User user = new User(
-                "alberto",
-                "1234",
-                "Alberto",
-                "Bollo", UserType.ESTUDIANTE);
-
-        SessionFactory sf = new Configuration().configure().buildSessionFactory();
-
-        Session session = sf.openSession();
-
-        Transaction transaction = session.beginTransaction();
-
-        session.persist(user);
-
-        transaction.commit();
-
-        session.close();
-        sf.close();
 
     }
 }
